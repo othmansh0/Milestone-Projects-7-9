e4e7c49b6d6761447da57bcfae81c6fa6c213918
Othman Shahrouri <othmanshahrouri@gmail.com>
2021-08-23 18:18:25 +0300

    Load words/clues and split them

--- Milestone-Projects 7-9/ViewController.swift
+++ Milestone-Projects 7-9/ViewController.swift
@@ -8,11 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var level=0
+    var words = [String]()
+    var clues = [String]()
+    
+    var currentWord:String!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if let path = Bundle.main.url(forResource: "level1", withExtension: "txt") {
+            if let allWords = try? String(contentsOf: path){
+               // print(allWords)
+                var lines = allWords.components(separatedBy: "\n")
+                //shuffle lines before splitting them into clues and words
+                lines.shuffle()
+                for line in lines{
+                    let parts = line.components(separatedBy: ": ")
+                    words.append(parts[0])
+                    clues.append(parts[1])
+                    
+                }
+                print(words)
+                print(clues)
+                
+                
+            }
+        }
+        
+    
+    
     }
+    
 
 
 }
